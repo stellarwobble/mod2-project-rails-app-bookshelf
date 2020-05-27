5b9cfdd8f7b8b8f3b123e74410a6d5e0178d7e96
stellarwobble <jennidemuir@gmail.com>
2020-05-27 09:54:56 -0700

    seeded database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,50 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+UserBookReview.destroy_all
+Book.destroy_all
+Author.destroy_all
+Genre.destroy_all
+User.destroy_all
+
+
+
+
+100.times do
+    Author.create!({
+        name: Faker::Book.author
+    })
+
+
+end
+
+15.times do
+    Genre.create!({
+        name: Faker::Book.genre
+    })
+end
+
+200.times do
+    Book.create!({
+        title: Faker::Book.title,
+        author_id: Author.all.sample.id,
+        genre_id: Genre.all.sample.id
+    })
+end
+
+50.times do 
+    User.create!({
+        name: Faker::Name.first_name
+    })
+end
+
+50.times do
+    UserBookReview.create!({
+        review: Faker::Hipster.paragraph,
+        rating: rand(1..10),
+        user_id: User.all.sample.id,
+        book_id: Book.all.sample.id
+
+    })
+end
