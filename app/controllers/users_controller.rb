a4436d027d8aefa38d999f79b699d4a5c8c15c65
stellarwobble <jennidemuir@gmail.com>
2020-05-27 17:13:05 -0700

    views and forms

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,26 @@
 class UsersController < ApplicationController
+
+    def show
+        @user = User.find(params[:id])
+
+    end
+
+    def new
+        @user = User.new
+
+    end
+
+    def create
+        @user = User.create(user_params)
+        return redirect_to controller: 'users', action: 'new' unless @user.save
+        session[:user_id] = @user.id
+        redirect_to controller: 'sessions', action: 'new'
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :password, :password_confirmation)
+    end
+
 end
